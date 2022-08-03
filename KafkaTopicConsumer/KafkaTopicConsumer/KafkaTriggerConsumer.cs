0e244f607fa830b34134ab29d5657982f33e5a5d
Jake Ellingson <jake.ellingson@meijer.com>
2022-08-03 11:58:37 -0500

    POC for Kafka messaging

---
+++ KafkaTopicConsumer/KafkaTopicConsumer/KafkaTriggerConsumer.cs
@@ -0,0 +1,32 @@
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Kafka;
+using Microsoft.Extensions.Logging;
+
+namespace KafkaTopicConsumer
+{
+    public class KafkaTriggerConsumer
+    {
+        // KafkaTrigger sample 
+        // Consume the message from "topic" on the LocalBroker.
+        // Add `BrokerList` and `KafkaPassword` to the local.settings.json
+        // For EventHubs
+        // "BrokerList": "{EVENT_HUBS_NAMESPACE}.servicebus.windows.net:9093"
+        // "KafkaPassword":"{EVENT_HUBS_CONNECTION_STRING}
+        [FunctionName("KafkaTriggerConsumer")]
+        public void Run(
+            [KafkaTrigger("BrokerList",
+                          "purchases",
+                          Username = "ConfluentCloudUserName",
+                          Password = "ConfluentCloudPassword",
+                          Protocol = BrokerProtocol.SaslSsl,
+                          AuthenticationMode = BrokerAuthenticationMode.Plain,
+                          ConsumerGroup = "$Default")] KafkaEventData<string>[] events,
+            ILogger log)
+        {
+            foreach (KafkaEventData<string> eventData in events)
+            {
+                log.LogInformation($"C# Kafka trigger function processed a message: {eventData.Value}");
+            }
+        }
+    }
+}
